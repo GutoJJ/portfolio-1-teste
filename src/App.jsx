b9e5e85b5dbbf430303e72f22dccb05871bf7651
GutoJJ <augusto-jung@educar.rs.gov.br>
2023-09-03 22:26:31 -0300

    Midias

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import id_img from './img/ID Verified.png';
 import cake_img from './img/cake.png';
 import CSS_img from './img/CSS3.png';
 import instagram from './img/insta.png';
+import discord from './img/discord.png';
+import github from './img/GitHub.png';
 
 function App() {
 
@@ -10,7 +12,7 @@ function App() {
       <div className="app">
 
 
-        <div className="container">
+        <div className="container" id='wrapper'>
 
           <div className="row" id="profile">
             <div className="card" id="pfp"></div>
@@ -38,13 +40,24 @@ function App() {
               <img src={instagram} alt="" />
               <a href="https://www.instagram.com/guto.jung/">@guto.jung</a>
             </a>
-            <div className="card-midia"></div>
-            <div className="card-midia"></div>
+            <a href = '/'className="card-midia" id='discord'>
+              <img src={discord} alt="" />
+              <a href="">.naydrus</a>
+            </a>
+            <a href='https://github.com/GutoJJ' className="card-midia" id='github'>
+              <img src={github} alt="" />
+              <a href="https://github.com/GutoJJ">GutoJJ</a>
+            </a>
 
           </div>
 
-          <div className="row" id="bio">
+          <div className="container" id="bio">
+              <h2>Olá, eu sou o augusto 👋 </h2>
+              <p>Descobri minha paixão em design e programação web ainda novo. Posso não ser o melhor no que faço, mas me orgulho dos meus designs e sites, foco sempre em fazê-los com meu maior carinho!</p>
+          </div>
 
+          <div id='titulo-linguagens'>
+              <h1>- Linguagens -</h1>
           </div>
 
         </div>
